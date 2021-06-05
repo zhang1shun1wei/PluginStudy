2d8f08590535ae359c822a8f28bd5ccd4c1297a6
zhangshunwei <zhangshunwei@meituan.com>
2021-06-05 18:07:16 +0800

    仓库倒入成功
    Signed-off-by: zhangshunwei <zhangshunwei@meituan.com>

--- mylibrary/src/main/groovy/com/davisplugins/PluginImpl.groovy
+++ mylibrary/src/main/groovy/com/davisplugins/PluginImpl.groovy
@@ -1,14 +1,38 @@
 package com.davisplugins
 
+import com.android.build.api.transform.QualifiedContent
+import com.android.build.api.transform.Transform
 import com.android.build.gradle.AppExtension
+import com.android.build.gradle.internal.pipeline.TransformManager
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-class PluginImpl implements Plugin<Project>{
+class PluginImpl extends Transform implements Plugin<Project> {
 
-    void apply(Project project){
-        System.out.println("========================");
-        System.out.println("hello gradle plugin!");
-        System.out.println("========================");
+    void apply(Project project) {
+        System.out.println("***********************");
+        def androidRegiste = project.extensions.findByType(AppExtension)
+        androidRegiste.registerTransform(this)
+        System.out.println("zsw11111 = "+androidRegiste.toString())
+    }
+
+    @Override
+    String getName() {
+        return "LifecyclePlugin"
+    }
+
+    @Override
+    Set<QualifiedContent.ContentType> getInputTypes() {
+        return TransformManager.CONTENT_CLASS
+    }
+
+    @Override
+    Set<? super QualifiedContent.Scope> getScopes() {
+        return TransformManager.SCOPE_FULL_PROJECT
+    }
+
+    @Override
+    boolean isIncremental() {
+        return false
     }
 }
